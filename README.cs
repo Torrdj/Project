7a72d334f10d848e15a35ae13d3c557629d670ee
Tordj <leo.tordjman@epita.fr>
2015-12-06 19:13:03 +0100

    modif README

--- README.cs
+++ README.cs
@@ -6,18 +6,21 @@ class Personnage
         Vie Max = 1000
         Mana Max = 1000
         Attaque = 50
-        Vitesse d'Attaque = 1000
-        Defence = 20
+        Vitesse d'Attaque = 1000 ms
+        Defense = 20
+
+        Attaque : 
+            - Coup de molette
     */
 }
 class Server /// : Personnage
 {
     /*
         Vie max *= 1.5 -> 1500
-        Defence *= 1.2 -> 24
-        Vitesse d'Attaque *= 1.25 -> 1250
+        Defense *= 1.2 -> 24
+        Vitesse d'Attaque *= 1.25 -> 1250 ms
 
-        Attaque :
+        Attaques :
             - DDOS
             - Firewall
     */
@@ -35,7 +38,7 @@ class Laptop /// : Personnage
 {
     /*
         Vie Max *= 0,9 -> 900
-        Vitesse d'Attaque *= 0,5 -> 500
+        Vitesse d'Attaque *= 0,5 -> 500 ms
         Attaque *= 0,9 -> 45
 
         Attaque : 
