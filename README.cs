d5eeee7fbec8146f180f771843b37d3ce1b20060
Arbodgad <damien.rioual@epita.fr>
2015-12-06 15:31:35 +0100

    Mise a jour README

--- README.cs
+++ README.cs
@@ -3,24 +3,42 @@ using System;
 class Personnage
 {
 	/*
-        Informations de la classe
+        Vie Max = 1000
+        Mana Max = 1000
+        Attaque = 50
+        Vitesse d'Attaque = 1000
+        Defence = 20
     */
 }
 class Server /// : Personnage
 {
     /*
-        Informations de la classe
+        Vie max *= 1.5 -> 1500
+        Defence *= 1.2 -> 24
+        Vitesse d'Attaque *= 1.25 -> 1250
+
+        Attaque :
+            - DDOS
+            - Firewall
     */
 }
 class Computer /// : Personnage
 {
     /*
-        Informations de la classe
+        Attaque *= 1,2 -> 60
+
+        Attaque : 
+            - FailureSystem
     */
 }
 class Laptop /// : Personnage
 {
     /*
-        Informations de la classe
+        Vie Max *= 0,9 -> 900
+        Vitesse d'Attaque *= 0,5 -> 500
+        Attaque *= 0,9 -> 45
+
+        Attaque : 
+            - Turbo Boost
     */
 }
