05cb340c61059c94db783fab5fc5fbc2a6cf4519
Tordj <leo.tordjman@epita.fr>
2015-12-06 13:46:04 +0100

    ajout du README

---
+++ README.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Personnage
+{
+	/*
+        Informations de la classe
+    */
+}
+class Server /// : Personnage
+{
+    /*
+        Informations de la classe
+    */
+}
+class Computer /// : Personnage
+{
+    /*
+        Informations de la classe
+    */
+}
+class Laptop /// : Personnage
+{
+    /*
+        Informations de la classe
+    */
+}
