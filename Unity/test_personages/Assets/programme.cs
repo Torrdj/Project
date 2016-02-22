a76b65018c5d2fa26db306eb6cb5712a611e9401
Torrdj <leo.tordjman@epita.fr>
2016-02-22 16:18:00 +0100

    y'a beaucoup de warning sur ce add...

---
+++ Unity/test_personages/Assets/programme.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class programme : MonoBehaviour {
+    public GameObject Perso;
+	// Use this for initialization
+	void Awake () {
+        Instantiate(Perso, transform.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
