a90de556920eac493280d66fce12987b699776a6
Arbodgad <damien.rioual@epita.fr>
2016-03-03 05:31:50 +0100

    ajout menu principal

---
+++ Unity/test_personages/Assets/Menu/Script/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public void OnGUI()
+    {
+        if (GUI.Button(Rect.MinMaxRect(Screen.width/2 - 100, Screen.height/2 - 50, Screen.width/2 + 200, Screen.height/2 + 100), "Start"))
+        {
+            Application.LoadLevel("test0");
+        }
+    }
+}
