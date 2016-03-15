3264baa0c6dc3de4bae2866c2c3c0eac04cb2208
Torrdj <leo.tordjman@epita.fr>
2016-03-15 04:32:42 +0100

    ajout du son d'ambiance dans first

--- Unity/new_test/Assets/Scripts/menu0.cs
+++ Unity/new_test/Assets/Scripts/menu0.cs
@@ -27,10 +27,14 @@ public class menu0 : MonoBehaviour {
         //GUI.BeginGroup(mainMenu, "");
         //GUI.Box(Rect.MinMaxRect(0, 0, menuSizeX, menuSizeY), "");
 
-        //La demande de champs d'ip pour rejoindre un serveur
-        serverIP = GUI.TextField(new Rect(sizeButtonX + 30, 60, 120, 30), serverIP, 40);
+        //taille par defaut des boutons
+        const int buttonWidth = 100;
+        const int buttonHeight = 50;
 
-        if (GUI.Button(Rect.MinMaxRect(Screen.width - 300, 150, Screen.width - 150, 180 /*10, 20, sizeButtonX, sizeButtonY*/), "Créer serveur"))
+        //La demande de champs d'ip pour rejoindre un serveur
+        serverIP = GUI.TextField(new Rect(Screen.width / 2 - (120 / 2), (2 *Screen.height / 6) - (30 / 2), 120, 30), serverIP, 40);
+
+        if (GUI.Button(new Rect(Screen.width / 2 - ((buttonWidth + 20) / 2), (3 * Screen.height / 6) - (buttonHeight / 2), buttonWidth + 20, buttonHeight), "Créer serveur"))
         {
             //Création du serveur
             instantiatedMaster = (GameObject)Instantiate(networkMaster, Vector3.zero, Quaternion.identity);
@@ -38,7 +42,7 @@ public class menu0 : MonoBehaviour {
             scriptStartNet.server = true;
             scriptStartNet.listenPort = serverPort;
         }
-        if (GUI.Button(Rect.MinMaxRect(150, 180, 300, 210 /*10, 60, sizeButtonX, sizeButtonY*/), "Rejoindre serveur"))
+        if (GUI.Button(new Rect(Screen.width / 2 - ((buttonWidth + 50) / 2), (4 * Screen.height / 6) - (buttonHeight / 2), buttonWidth + 50, buttonHeight), "Rejoindre serveur"))
         {
             //Rejoindre serveur
             instantiatedMaster = (GameObject)Instantiate(networkMaster, Vector3.zero, Quaternion.identity);
@@ -49,15 +53,12 @@ public class menu0 : MonoBehaviour {
         }
         //GUI.EndGroup();
 
-        //taille par defaut des boutons
-        const int buttonWidth = 100;
-        const int buttonHeight = 50;
         
         //bouton demarrage du jeu
-        if (GUI.Button(new Rect(Screen.width / 2 - (buttonWidth / 2), (2 * Screen.height / 3) - (buttonHeight / 2), buttonWidth, buttonHeight), "Start"))
+        if (GUI.Button(new Rect(Screen.width / 2 - (buttonWidth / 2), (5 * Screen.height / 6) - (buttonHeight / 2), buttonWidth, buttonHeight), "Start"))
         {
             //Application.LoadLevel("test0");
-            SceneManager.LoadScene("0");
+            SceneManager.LoadScene("first");
         }
     }
 }
