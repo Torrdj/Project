a5442c367c10f23608ec674190a58be5fcf59dd7
Arbodgad <damien.rioual@epita.fr>
2016-05-03 00:06:47 +0200

    ajout barre d'attaque

---
+++ Unity/Quantum_Quest/Assets/scripts/UI/bar_attack.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class bar_attack : MonoBehaviour {
+    
+    
+    public Image barattack;
+    public GUIStyle style_button;
+    public int buttonWidth;
+    public int buttonHeight;
+
+	// Use this for initialization
+	void Start () {
+    }
+
+    
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        barattack.transform.position = new Vector2(Screen.width / 2, 27);
+    }
+
+    void OnGUI ()
+    {
+        if (GUI.Button(new Rect((Screen.width / 2) - 297.5f, Screen.height - 50, buttonWidth, buttonHeight), "", style_button))
+        {
+
+        }
+    }
+
+
+
+}
