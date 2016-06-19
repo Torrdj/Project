8fb8d634e5dad5ca1875db0081da6ff5c5912631
Torrdj <leo.tordjman@epita.fr>
2016-06-19 03:09:39 +0200

    big commit -> projet final

---
+++ Unity/Quantum_Quest/Assets/Resources/scripts/GUIController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUIController : MonoBehaviour {
+
+    Text statusText, masterText;
+
+	// Use this for initialization
+	void Start () {
+        statusText = GameObject.Find("statusText").GetComponent<Text>();
+        masterText = GameObject.Find("masterText").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        statusText.text = "Status : " + PhotonNetwork.connectionStateDetailed.ToString();
+        masterText.text = "isMaster : " + PhotonNetwork.isMasterClient; 
+	}
+}
--- Unity/Quantum_Quest/Assets/scripts/PlayerInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class PlayerInfo : MonoBehaviour {
-
-    public string prefab_name;
-}
